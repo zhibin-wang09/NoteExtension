2fc3f9dececcdf61838a5ddc2a7dc9faedaf250d
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2026-01-24 23:11:50 -0500

    Add contextMenus to trigger add note

--- background.ts
+++ background.ts
@@ -5,3 +5,31 @@ console.log("NoteExtension background script loaded");
 chrome.runtime.onInstalled.addListener(() => {
   console.log("NoteExtension installed");
 });
+
+chrome.contextMenus.create(
+  {
+    title: "Add Note",
+    visible: true,
+    id: "add_note",
+    contexts: ["all"],
+    documentUrlPatterns: ["<all_urls>"],
+  },
+  () => {
+    console.log("Context menu item 'Add Note' created");
+  },
+);
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "add_note") {
+    // call the content script to create a pop up to add note
+    console.log(tab);
+    chrome.tabs.sendMessage(
+      tab!.id!,
+      { action: "open_note_taker" },
+      (response) => {
+        console.log(response)
+      },
+    );
+  }
+  return true;
+});
--- content.ts
+++ content.ts
@@ -1,1 +1,13 @@
-console.log("Note Extension content script injected")
+import { stat } from "fs";
+
+console.log("Note Extension content script injected");
+
+// Content.ts file is a script file that allows us interact with the web page (essentially DOM elements)
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "open_note_taker") {
+    console.log("note taker opened");
+  }
+  sendResponse({ statusCode: 200 });
+  return true;
+});
