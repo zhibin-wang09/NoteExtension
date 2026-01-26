6eda35248c898f0027235dbc1b97bcade9762ccc
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2026-01-25 23:22:00 -0500

    Migrate code into react content script. Keeping background as vanilla ts because it's a DOM modification file

--- src/content/main.tsx
+++ src/content/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './views/App'
 
 console.log("Content script loaded");
 const container = document.createElement("div");
@@ -8,9 +9,6 @@ document.body.appendChild(container);
 
 createRoot(container).render(
   <StrictMode>
-    <div className="p-4 bg-white border border-gray-300 rounded shadow-lg note-extension-popup">
-      <h2 className="text-lg font-semibold mb-2">Note Extension</h2>
-      <p className="text-sm text-gray-600">This is the content script popup UI.</p>
-    </div>
+    <App/>
   </StrictMode>,
 )
---
+++ src/content/views/App.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  useEffect(() => {
+    const messageListener = (request: any, _: any, sendResponse: any) => {
+      if (request.action === "open_note_taker") {
+        // render a box to take note on the top right corner of the selected text
+        // check if the highlighted text is an image or video or text
+
+        if (request.mediaType === "image" || request.mediaType === "video") {
+          alert("Media note taking is not supported yet.");
+          sendResponse({
+            statusCode: 400,
+            message: "Media note taking not supported",
+          });
+          return;
+        }
+        
+        sendResponse({ statusCode: 200 });
+      }
+      return true;
+    };
+
+    chrome.runtime.onMessage.addListener(messageListener);
+
+    return () => {
+      chrome.runtime.onMessage.removeListener(messageListener);
+    };
+  }, []);
+
+  return <div>Hello, Note Extension!</div>;
+}
+
+export default App;
