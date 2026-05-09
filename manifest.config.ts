9f72be57f92f1712104635582e552169390b4f00
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2026-05-08 22:50:28 -0400

    update manifest version

--- manifest.config.ts
+++ manifest.config.ts
@@ -3,7 +3,7 @@ import { defineManifest } from "@crxjs/vite-plugin";
 export default defineManifest({
   manifest_version: 3,
   name: "Post-It-Note",
-  version: "1.1",
+  version: "1.3.0",
   description: "A Note Taking Extension",
   permissions: ["tabs", "contextMenus", "storage"],
   icons: {
